d7660652329128db5f0d0554a6742710f46fc8c6
John Johnson <jj_email.email.com>
2022-03-04 00:16:54 +0500

    Add a comment

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ number1 = input("First number: ")
 number2 = input("Second number: ")
 
 sum = float(number1) + float(number2)
+
+# Display the sum
 print("\nThe sum of {0} and {1} is {2}" .format(number1, number2, sum))
